37c07a31cac89e1eb1ede1d90f79f4b11f5f33e8
Marc Schmitz <marcschmitz333@gmail.com>
2016-02-02 12:33:14 +0100

    Add random color, tail function and responsive resolution line.

--- sketch.js
+++ sketch.js
@@ -5,6 +5,11 @@ var padWidth = parseInt($('#pad').css('width'), 10);
 var resVal = 60;
 var newResVal = 60;
 var size = padWidth / resVal;
+var paintMethod = 0; // 0: black, 1: rand, 2: tail
+var black = '#000';
+var rndColor = getRandomColor();
+var backgrColor = $('#pad').css('background-color');
+var tail = 0;
 
 // function preparing the Pad
 function makePixels(resVal, size) {
@@ -25,9 +30,35 @@ function removePixels() {
   $('.pixel').remove();
 }
 
+// function computing a random color
+function getRandomColor() {
+  // convert int to Hex-String: .toString(16)
+  // max int-number: 16777215
+  var n = Math.floor(Math.random() * (16777215 + 1));
+  return "#" + n.toString(16);
+}
 
-$(document).ready(makePixels(resVal, size));
 
+$(document).ready( function() {
+  makePixels(resVal, size)
+  $('#lines').text('30');
+});
+
+$('#pad').on('mouseenter', '.pixel', function() {
+  var pixel = $(this);
+  if (paintMethod === 0) {
+    pixel.css({'background-color':black}, 'fast');
+  } else if (paintMethod === 1) {
+    pixel.css({'background-color':rndColor}, 'fast');
+    rndColor = getRandomColor();
+  } 
+  // tail
+  if (tail) {
+    setTimeout(function() {
+      pixel.css({'background-color':backgrColor}, 'fast');
+    }, 1000);
+  }
+});
 
 /* THE CLEAR-BUTTON */
 $('#clear').on('click', function() {
@@ -36,7 +67,7 @@ $('#clear').on('click', function() {
     removePixels();
     makePixels(resVal, size);
   } else {
-    var color = $('#pad').css('background-color');
+    var color = backgrColor;
     $('.pixel').css( 'background-color', color );
   }
 });
@@ -45,14 +76,29 @@ $('#clear').on('click', function() {
 $('#resolution').on('change', function() {
   newResVal = +$('#resolution').val();
   size = padWidth / newResVal;
-  alert('PadWidth'+padWidth+'size '+size);
+  $('#lines').text((newResVal / 2).toString());
 });
 
-$('#pad').on('mouseenter', '.pixel', function() {
-  $(this).css({'background-color':'#000'}, 'fast');
+/* REGISTER COLORCHANGES */
+$('input:radio[name=color]').on('change', function() {
+  var val = $('input[name=color]:checked').val();
+  if (val === 'black') {
+    paintMethod = 0;
+  } else if (val === 'rand') {
+    paintMethod = 1;
+  } else {
+    paintMethod = 2;
+  }
 });
 
-
+/* REGISTER TAIL OPTION */
+$('input:checkbox[name=tail]').on('change', function() {
+  if ( $('input[name=tail]').is(':checked') ) {
+    tail = 1;
+  } else {
+    tail = 0;
+  }
+});
 
 
 
